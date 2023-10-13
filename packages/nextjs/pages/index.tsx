ebf6ec377a39446c2c312ddca9bf2be3a99ca2c8
Shiv Bhonde <shivbhonde04@gmail.com>
2023-10-13 15:19:02 +0530

    update head tag

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,19 +1,15 @@
+import Head from "next/head";
 import type { NextPage } from "next";
-import { MetaHeader } from "~~/components/MetaHeader";
 import { ContractData } from "~~/components/example-ui/ContractData";
 import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
   return (
     <>
-      <MetaHeader
-        title="Example UI | Scaffold-ETH 2"
-        description="Example UI created with 🏗 Scaffold-ETH 2, showcasing some of its features."
-      >
-        {/* We are importing the font this way to lighten the size of SE2. */}
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
-      </MetaHeader>
+      </Head>
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
         <ContractInteraction />
         <ContractData />
